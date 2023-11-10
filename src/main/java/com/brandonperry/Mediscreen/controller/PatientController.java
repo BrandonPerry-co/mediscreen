b8fc687d35d4dc6b119f38cd130809c6c383401e
BrandonPerry-co <brandonperrymba@gmail.com>
2023-11-10 07:38:28 -0600

    Added method to only return one patient by Id.

--- src/main/java/com/brandonperry/Mediscreen/controller/PatientController.java
+++ src/main/java/com/brandonperry/Mediscreen/controller/PatientController.java
@@ -4,25 +4,24 @@ package com.brandonperry.Mediscreen.controller;
 import com.brandonperry.Mediscreen.entity.Patient;
 import com.brandonperry.Mediscreen.repo.PatientRepo;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/patient")
+@Slf4j
 public class PatientController {
     @Autowired
     private PatientRepo patientRepo;
 
     @GetMapping("/")
+    @ResponseBody
     public List<Patient> index(Model model) {
         List<Patient> patients = patientRepo.findAll();
         model.addAttribute("patients", patients);
@@ -30,20 +29,14 @@ public class PatientController {
     }
 
     @GetMapping("/findAll")
+    @ResponseBody
     public List<Patient> getAllPatients() {
         return patientRepo.findAll();
     }
 
-    @GetMapping("/patientList")
-    public ModelAndView getPatients() {
-        String viewName = "patientList";
-        Map<String, Object> model = new HashMap<String, Object>();
-        model.put("numberOfPatients", "1234");
-        return new ModelAndView(viewName, model);
-    }
-
     //FIXME change requestmapping to Post mapping
     @PostMapping("/add")
+    @ResponseBody
     public Patient insert(@RequestParam String address, @RequestParam String dob, @RequestParam String family, @RequestParam String given, @RequestParam String sex, @RequestParam String phone, RedirectAttributes redirectAttributes) {
         Patient patient = Patient.builder().address(address).dob(dob).family(family).given(given).sex(sex).phone(phone).build();
         patientRepo.save(patient);
@@ -51,54 +44,43 @@ public class PatientController {
         return patient;
     }
 
-//    @PutMapping("/update/{id}")
-//    public ResponseEntity<Patient> updatePatient(@PathVariable Long id, @RequestParam String address, @RequestParam String dob, @RequestParam String family, @RequestParam String given, @RequestParam String sex, @RequestParam String phone, RedirectAttributes redirectAttributes) {
-//        Optional<Patient> patientOptional = patientRepo.findById(id);
-//        if (patientOptional.isPresent()) {
-//            Patient patient = patientOptional.get();
-//            patient.setFamily(family);
-//            patient.setGiven(given);
-//            patient.setDob(dob);
-//            patient.setSex(sex);
-//            patient.setAddress(address);
-//            patient.setPhone(phone);
-//            patientRepo.save(patient);
-//            return new ResponseEntity<>(patient, HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//    }
-
     //FIXME change requestmapping to Post mapping
     @PutMapping("/update/{id}")
-    public Patient update(@PathVariable Long id,
-                          HttpServletRequest request,
-                          RedirectAttributes redirectAttributes) throws Exception {
+    @ResponseBody
+        public Patient update(@PathVariable Long id, @RequestBody Patient updatedPatient) throws Exception {
+        Optional<Patient> optionalPatient = patientRepo.findById(id);
+        Patient patient = null;
+        Patient savedPatient = null;
+        if (optionalPatient.isPresent()) {
+            patient = optionalPatient.get();
+log.error(updatedPatient.getAddress());
+            if (updatedPatient.getAddress() != null) patient.setAddress(updatedPatient.getAddress());
+            if (updatedPatient.getDob() != null) patient.setDob(updatedPatient.getDob());
+//            if (updatedPatient.getFamily() != null) patient.setFamily(updatedPatient.getFamily());
+//            if (given != null) patient.setGiven(given);
+//            if (sex != null) patient.setSex(sex);
+//            if (phone != null) patient.setPhone(phone);
 
-        String address = request.getParameter("address");
-        String dob = request.getParameter("dob");
-        String family = request.getParameter("family");
-        String given = request.getParameter("given");
-        String sex = request.getParameter("sex");
-        String phone = request.getParameter("phone");
+            savedPatient = patientRepo.save(patient);
+        } else {
+            throw new Exception("Patient not found!");
+        }
+        log.error(savedPatient.toString());
+        return savedPatient;
+    }
 
+
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Patient getPatient(@PathVariable Long id) throws Exception {
         Optional<Patient> optionalPatient = patientRepo.findById(id);
         Patient patient = null;
         if (optionalPatient.isPresent()) {
             patient = optionalPatient.get();
-
-            if (address != null) patient.setAddress(address);
-            if (dob != null) patient.setDob(dob);
-            if (family != null) patient.setFamily(family);
-            if (given != null) patient.setGiven(given);
-            if (sex != null) patient.setSex(sex);
-            if (phone != null) patient.setPhone(phone);
-
-            patientRepo.save(patient);
         } else {
             throw new Exception("Patient not found!");
         }
-
         return patient;
     }
 }
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,106 +1,56 @@
 //Fetching all the Patients
-document.addEventListener("DOMContentLoaded", function() {
-    fetchAllPatients();
-});
-
-function fetchAllPatients() {
-    fetch('http://localhost:8081/patient/findAll')
-    .then(response => response.json())
-    .then(data => {
-        let patientsList = document.createElement('ul');
-
-        data.forEach(patient => {
-            let listItem = document.createElement('li');
-            listItem.textContent = `${patient.given} ${patient.family} (${patient.dob}) - ${patient.address}, ${patient.phone}, ${patient.sex}`;
-            patientsList.appendChild(listItem);
-        });
-
-        document.body.appendChild(patientsList);
-    });
-}
 
 
-//Updating the Patient
-document.addEventListener("DOMContentLoaded", function() {
-    updatePatient();
-});
-
-function updatePatient(id) {
-    let address = document.getElementById('address').value;
-    let dob = document.getElementById('dob').value;
-    let family = document.getElementById('family').value;
-    let given = document.getElementById('given').value;
-    let sex = document.getElementById('sex').value;
-    let phone = document.getElementById('phone').value;
-
-    let payload = {
-        address: address,
-        dob: dob,
-        family: family,
-        given: given,
-        sex: sex,
-        phone: phone
-    };
-
-    fetch(`http://localhost:8081/patient/update/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-    })
-    .then(response => response.json())
-    .then(data => {
-        location.reload();
-    });
-}
-
-//// Posting A New Patient
-//document.addEventListener("DOMContentLoaded", function() {
-//    const form = document.getElementById('mediscreenForm');
-//    form.addEventListener('submit', handleSubmit);
-//});
-//
-//function handleSubmit(event) {
-//    event.preventDefault(); // Prevent default form submission
-//
-//    let address = document.getElementById('address').value;
-//    let dob = document.getElementById('dob').value;
-//    let family = document.getElementById('family').value;
-//    let given = document.getElementById('given').value;
-//    let sex = document.getElementById('sex').value;
-//    let phone = document.getElementById('phone').value;
-//
-//    let payload = {
-//        address: address,
-//        dob: dob,
-//        family: family,
-//        given: given,
-//        sex: sex,
-//        phone: phone
-//    };
-//
-//    // Make the POST request using Fetch API
-//    fetch('http://localhost:8081/patient/add', {
-//        method: 'POST',
-//        headers: {
-//            'Content-Type': 'application/x-www-form-urlencoded'
-//        },
-//        body: new URLSearchParams(payload)
-//    })
+//function fetchAllPatients() {
+//    fetch('http://localhost:8081/patient/findAll')
 //    .then(response => response.json())
 //    .then(data => {
-//        console.log(data);
-//        alert('Patient successfully added!');
-//    })
-//    .catch(error => {
-//        console.error('Error:', error);
-//        alert('Failed to add patient.');
+//        let patientsList = document.createElement('ul');
+//
+//        data.forEach(patient => {
+//            let listItem = document.createElement('li');
+//            listItem.textContent = `${patient.given} ${patient.family} (${patient.dob}) - ${patient.address}, ${patient.phone}, ${patient.sex}`;
+//            patientsList.appendChild(listItem);
+//        });
+//
+//        document.body.appendChild(patientsList);
 //    });
 //}
 
+// Updating patient
+//function updatePatient(id) {
+//    const fieldValue = document.getElementById(id).value;
+//
+//    if(!fieldValue) {
+//        alert(`Please enter a value for ${id} before updating.`);
+//        return;
+//    }
+//
+//    fetch(`http://localhost:8081/patient/update/${id}`, {
+//        method: 'POST',
+//        headers: {
+//            'Content-Type': 'application/json',
+//        },
+//        body: JSON.stringify({
+//            field: fieldId,
+//            value: fieldValue
+//        })
+//    })
+//    .then(response => response.json())
+//    .then(data => {
+//        if(data.success) {
+//            alert(`Successfully updated ${fieldId}!`);
+//        } else {
+//            alert(`Failed to update ${fieldId}. Please try again.`);
+//        }
+//    })
+//    .catch(error => {
+//        console.error(`Error updating ${fieldId}:`, error);
+//        alert(`An error occurred while updating ${fieldId}. Please try again.`);
+//    });
+//}
 
-// Posting A New Patient
+// Posting A New Patient this works
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById('mediscreenForm');
     form.addEventListener('submit', handleSubmit);
@@ -144,16 +94,129 @@ function handleSubmit(event) {
 
 
 // Ensure the DOM is loaded before attaching event listeners
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById('mediscreenUpdateForm');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        let patientId = document.getElementById('patientId').value;
-        updatePatient(patientId);
-    });
+//document.addEventListener("DOMContentLoaded", function() {
+//    const form = document.getElementById('mediscreenUpdateForm');
+//    form.addEventListener('submit', function(event) {
+//        event.preventDefault();
+//        let patientId = document.getElementById('id').value;
+//        updatePatient(id);
+//    });
+//});
+document.getElementById('mediscreenUpdateForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    updatePatient();
 });
 
-function updatePatient(id) {
+//function updatePatient(id) {
+//    let id = document.getElementById('id').value;
+//    let address = document.getElementById('address').value;
+//    let dob = document.getElementById('dob').value;
+//    let family = document.getElementById('family').value;
+//    let given = document.getElementById('given').value;
+//    let sex = document.getElementById('sex').value;
+//    let phone = document.getElementById('phone').value;
+//
+////    let payload = {
+////        address: address,
+////        dob: dob,
+////        family: family,
+////        given: given,
+////        sex: sex,
+////        phone: phone
+////    };
+//
+//      Object.keys(userDetails).forEach(key => {
+//                if (!userDetails[key]) delete userDetails[key];
+//            });
+//
+//    fetch(`http://localhost:8081/patient/update/${id}`, {
+//        method: 'PUT',
+//        headers: {
+//            'Content-Type': 'application/json'
+//        },
+//        body: JSON.stringify(userDetails)
+//    })
+//    .then(response => response.json())
+//    .then(data => {
+//        // To display the updated patient information
+//        let infoDiv = document.getElementById('updatedPatientInfo');
+//        infoDiv.innerHTML = `
+//            Name: ${data.given} ${data.family}<br>
+//            DOB: ${data.dob}<br>
+//            Address: ${data.address}<br>
+//            Sex: ${data.sex}<br>
+//            Phone: ${data.phone}
+//        `;
+//    })
+//    .catch(error => {
+//        console.error('Error:', error);
+//    });
+//}
+//
+//document.addEventListener("DOMContentLoaded", function() {
+//    fetchPatients();
+//});
+
+
+//window.updatePatient = updatePatient;
+//
+//function updatePatient(idElem) {
+//let idElem = document.getElementById('id');
+//    let address = document.getElementById('address').value;
+//    let dob = document.getElementById('dob').value;
+//    let family = document.getElementById('family').value;
+//    let given = document.getElementById('given').value;
+//    let sex = document.getElementById('sex').value;
+//    let phone = document.getElementById('phone').value;
+//
+//    let userDetails = {
+//        address: address,
+//        dob: dob,
+//        family: family,
+//        given: given,
+//        sex: sex,
+//        phone: phone
+//    };
+//
+//    // Clean up empty fields
+//    Object.keys(userDetails).forEach(key => {
+//        if (!userDetails[key]) delete userDetails[key];
+//    });
+//
+//    fetch(`http://localhost:8081/patient/update/${id}`, {
+//        method: 'PUT',
+//        headers: {
+//            'Content-Type': 'application/json'
+//        },
+//        body: JSON.stringify(userDetails)
+//    })
+//    .then(response => response.json())
+//    .then(data => {
+//        // Display the updated patient information
+//        let infoDiv = document.getElementById('updatedPatientInfo');
+//        infoDiv.innerHTML = `
+//            Name: ${data.given} ${data.family}<br>
+//            DOB: ${data.dob}<br>
+//            Address: ${data.address}<br>
+//            Sex: ${data.sex}<br>
+//            Phone: ${data.phone}
+//        `;
+//    })
+//    .catch(error => {
+//        console.error('Error:', error);
+//    });
+//}
+//
+//document.addEventListener("DOMContentLoaded", function() {
+//    // Assuming fetchPatients() is a function that populates some initial data.
+//    fetchPatients();
+//});
+
+window.updatePatient = updatePatient;
+
+function updatePatient() {
+console.log('updatePatient function called');
+    let id = document.getElementById('id').value;
     let address = document.getElementById('address').value;
     let dob = document.getElementById('dob').value;
     let family = document.getElementById('family').value;
@@ -161,7 +224,7 @@ function updatePatient(id) {
     let sex = document.getElementById('sex').value;
     let phone = document.getElementById('phone').value;
 
-    let payload = {
+    let userDetails = {
         address: address,
         dob: dob,
         family: family,
@@ -170,16 +233,19 @@ function updatePatient(id) {
         phone: phone
     };
 
+    Object.keys(userDetails).forEach(key => {
+        if (!userDetails[key]) delete userDetails[key];
+    });
+
     fetch(`http://localhost:8081/patient/update/${id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(payload)
+        body: JSON.stringify(userDetails)
     })
     .then(response => response.json())
     .then(data => {
-        // Display the updated patient information below the h2 tag
         let infoDiv = document.getElementById('updatedPatientInfo');
         infoDiv.innerHTML = `
             Name: ${data.given} ${data.family}<br>
@@ -198,6 +264,37 @@ document.addEventListener("DOMContentLoaded", function() {
     fetchPatients();
 });
 
+
+// Fetch all Patients
+//function fetchPatients() {
+//    fetch("http://localhost:8081/patient/findAll")
+//    .then(response => {
+//        if (!response.ok) {
+//            throw new Error("Network response was not ok");
+//        }
+//        return response.json();
+//    })
+//    .then(patients => {
+//        const patientsList = document.getElementById("patientsList");
+//
+//        patients.forEach(patient => {
+//            const patientInfo = document.createElement("div");
+//            patientInfo.textContent = `
+//                ID: ${patient.id}
+//                Address: ${patient.address},
+//                DOB: ${patient.dob},
+//                Family: ${patient.family},
+//                Given: ${patient.given},
+//                Sex: ${patient.sex},
+//                Phone: ${patient.phone}
+//            `;
+//            patientsList.appendChild(patientInfo);
+//        });
+//    })
+//    .catch(error => {
+//        console.error("There was a problem with the fetch operation:", error.message);
+//    });
+//}
 function fetchPatients() {
     fetch("http://localhost:8081/patient/findAll")
     .then(response => {
@@ -209,9 +306,16 @@ function fetchPatients() {
     .then(patients => {
         const patientsList = document.getElementById("patientsList");
 
+        // Check if patientsList exists
+        if (!patientsList) {
+            console.warn("Element with ID 'patientsList' not found in the DOM.");
+            return;
+        }
+
         patients.forEach(patient => {
             const patientInfo = document.createElement("div");
             patientInfo.textContent = `
+                ID: ${patient.id}
                 Address: ${patient.address},
                 DOB: ${patient.dob},
                 Family: ${patient.family},
